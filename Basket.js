2fd93eb7da3ed08ae8114dd53633e8833bb3d98d
marian37 <opiela.marian@gmail.com>
2015-10-03 18:15:29 +0200

    Marian Opiela HW1

--- Basket.js
+++ Basket.js
@@ -1,4 +1,4 @@
-var prodcuts = [{
+var products = [{
 	name : "test",
 	price : 12.9,
 	inventory : 20
@@ -9,27 +9,92 @@ var prodcuts = [{
 }];
 
 function ProductLineItem(product) {
-	//implement
+	this.name = product.name;
+	this.quantity = 1;
 }
 
 ProductLineItem.prototype = {
-	//implement
+	getID : function() {
+		for (var i = 0; i < products.length; i++) {
+			if (this.name == products[i].name) {
+				return i;
+			}
+		}
+		return -1;
+	}	
 };
 
 var basket = (function(){
-	//implement
+	var productsInBasket = [];		
+
 	return {
+		findProductIDInBasket : function(productID) {
+			for (var i = 0; i < productsInBasket.length; i++) {
+				if (productID == productsInBasket[i].getID()) {
+					return i;
+				}
+			}
+			return -1;
+		},		
 		addProduct : function(productID){
-			//implement
+			if (productID >= products.length || productID < 0) {
+				throw Error("ProductID is not valid.");
+			}
+			if (products[productID].inventory == 0) {
+				throw Error("Product is out of stock.");
+			}
+						
+			products[productID].inventory--;
+
+			var IDInBasket = this.findProductIDInBasket(productID);
+			if (IDInBasket == -1) {
+				var item = new ProductLineItem(products[productID]);
+				productsInBasket.push(item);
+			} else {
+				productsInBasket[IDInBasket].quantity++;				
+			}
 		},
 		removeProduct : function(productID){
-			//implement
+			var IDInBasket = this.findProductIDInBasket(productID);
+			if (IDInBasket == -1) {
+				throw Error("The product is not in the basket.");
+			} else {
+				var quantity = productsInBasket[IDInBasket].quantity;
+				productsInBasket[IDInBasket].quantity = 0;
+				products[productID].inventory += quantity;
+			}	
 		},
 		updateProductQuantity : function(productID, quantity) {
-			//implement
+			if (productID >= products.length || productID < 0) {
+				throw Error("ProductID is not valid.");
+			}
+			if (quantity < 0) {
+				throw Error("Invalid quantity parameter.");
+			}
+			
+			var IDInBasket = this.findProductIDInBasket(productID);
+			if (IDInBasket == -1) {
+				if (products[productID].inventory < quantity) {
+					throw Error("There is not enough of this product in the inventory.");
+				}
+				this.addProduct(productID);
+				this.updateProductQuantity(quantity - 1);
+			} else {
+				var adding = quantity - productsInBasket[IDInBasket].quantity;
+				if (adding > 0 && products[productID].inventory < adding) {
+					throw Error("There is not enough of this product in the inventory.");
+				}
+				productsInBasket[IDInBasket].quantity += adding;
+				products[productID].inventory -= adding;
+			}				
 		},
 		getTotalPrice : function(){
-			//implement
-		}
+			var totalPrice = 0;
+			for (var i = 0; i < productsInBasket.length; i++) {
+				var productID = productsInBasket[i].getID();
+				totalPrice += productsInBasket[i].quantity * products[productID].price;
+			}
+			return totalPrice;
+		}		
 	}
 })();
